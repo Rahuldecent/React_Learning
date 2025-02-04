4918cb4aab7436684952fdd9c72568bf99ef4f28
Rahuldecent <rahulkeshari801@gmail.com>
2025-02-04 18:40:36 +0530

    changed in the app js

--- intro_react/react-project/src/main.jsx
+++ intro_react/react-project/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { Fragment, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-    // <App />
-    <Navbar/>
+    <Fragment>
+        <App />
+        <Navbar/>
+    </Fragment>
 )
