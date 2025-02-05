bc6b17758da8d34f30b65b291a89a067ced113c9
Rahuldecent <rahulkeshari801@gmail.com>
2025-02-05 10:33:05 +0530

    git config is done

--- intro_react/react-project/src/App.jsx
+++ intro_react/react-project/src/App.jsx
@@ -5,7 +5,7 @@ import UserCard from './components/UserCard'
 function App() {
   return (
     <div className='container'>
-    <UserCard/>
+    {/* <UserCard/> */}
     <UserCard/>
     <UserCard/>
 
