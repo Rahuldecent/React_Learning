ac3e7198f21e034884c0ce37f34e8a3c93881482
rahuldecent <rahulkeshari801@gmail.com>
2025-03-05 16:51:42 +0530

    add

---
+++ star-rating/src/components/StarRating/StarRating.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {FaStar} from 'react-icons/fa';
+
+export default  function StarRating({noOfStar =5}) {
+    const [rating,setRating] = useState(0);
+    const [hover,setHover] =useState(0);
+
+    function handleClick (getCurrentIndex) {
+
+    }
+    function handleMouseEnter(getCurrentIndex) {
+
+    }
+    function handleMouseLeave(getCurrentIndex) {
+
+    }
+    return  <div className="star-rating">
+        {
+            [...Array(noOfStar)].map((_,index) => {
+                index +=1
+                return <FaStar
+                key={index}
+                onClick={handleClick(index)}
+                onMouseMove={handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave(index)}
+                size={40}
+                />
+            })
+        }
+    </div>
+        
+    
+} 
