7493211f5520ed18a9b464e9fee45d753ffaecd4
rahuldecent <rahulkeshari801@gmail.com>
2025-05-06 12:51:37 +0530

    add

--- star-rating/src/components/StarRating/StarRating.jsx
+++ star-rating/src/components/StarRating/StarRating.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
-import {FaStar} from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
+import "./styles.css"
+export default function StarRating({ noOfStar = 8 }) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
 
-export default  function StarRating({noOfStar =5}) {
-    const [rating,setRating] = useState(0);
-    const [hover,setHover] =useState(0);
-
-    function handleClick (getCurrentIndex) {
-
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex)
     }
     function handleMouseEnter(getCurrentIndex) {
-
+        setHover(getCurrentIndex)
     }
-    function handleMouseLeave(getCurrentIndex) {
-
+    function handleMouseLeave() {
+        setHover(rating)
     }
-    return  <div className="star-rating">
+    return <div className="star-rating">
         {
-            [...Array(noOfStar)].map((_,index) => {
-                index +=1
+            [...Array(noOfStar)].map((_, index) => {
+                index += 1
                 return <FaStar
-                key={index}
-                onClick={handleClick(index)}
-                onMouseMove={handleMouseEnter(index)}
-                onMouseLeave={handleMouseLeave(index)}
-                size={40}
+                    key={index}
+                    className={index <= (hover || rating) ? 'active' : 'inactive'}
+                    onClick={()=> handleClick(index)}
+                    onMouseMove={() => handleMouseEnter(index)}
+                    onMouseLeave={() =>handleMouseLeave(index)}
+                    size={40}
                 />
             })
         }
     </div>
-        
-    
+
+
 } 
